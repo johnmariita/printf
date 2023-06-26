b49839fd96fd5290da009cf931377d05acc74e27
johnmariita <johnmariita421@gmail.com>
2023-06-27 00:13:46 +0300

    bin

--- main.h
+++ main.h
@@ -7,7 +7,7 @@ int _putchar(int c);
 int _printf(const char *format, ...);
 int print_str(char *string, int count);
 int print_num(int my_int, int count);
-int print_bin(int num, int count);
+int print_bin(unsigned int num, int count);
 int format_handler(char c, va_list ap, int count);
 int print_uns(unsigned int num, int count);
 int print_hex(unsigned int num, int count, int x);
